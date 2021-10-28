0f435821d62eee3a5d2fa0836ac71149b8b9419e
_MaxX_X <54334537+MaxGGx@users.noreply.github.com>
2021-10-28 17:55:24 -0300

    update Test2

--- M1/Test2/client.go
+++ M1/Test2/client.go
@@ -6,7 +6,7 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
-	"github.com/MaxGGx/Distribuidos-2021-2/tree/main/M1/Test2/chat"
+	"github.com/MaxGGx/Distribuidos-2021-2/M1/Test2/chat"
 )
 
 func main() {
