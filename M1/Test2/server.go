f74d3e19de97b9b9684a5c85346fa7cabf845207
_MaxX_X <54334537+MaxGGx@users.noreply.github.com>
2021-10-28 17:00:46 -0300

    update test2

--- M1/Test2/server.go
+++ M1/Test2/server.go
@@ -5,7 +5,12 @@ import (
 	"net"
 
 	"google.golang.org/grpc"
+	"github.com/MaxGGx/Distribuidos-2021-2/tree/main/M1/Test2/chat"
 )
+
+//Comando para inicializar el go.mod y go.sum, se le indica al comando donde estará alojado en el repo la carpeta con los archivos (o algo asi)
+//De esa forma los crea para que cuando se haga el commit, los vaya a ver allí también
+//go mod init github.com/MaxGGx/Distribuidos-2021-2/tree/main/M1/Test2
 
 func main() {
 	lis, err := net.Listen("tcp",":9000")
@@ -13,7 +18,11 @@ func main() {
 		log.Fatalf("No se puede escuchar en el puerto 9000: %v", err)
 	}
 
+	s := chat.Server{}
+
 	grpcServer := grpc.NewServer()
+
+	chat.RegisterChatServiceServer(grpcServer, &s)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Fallo en habilitar servidor gRPC en el puerto 9000: %v", err)
