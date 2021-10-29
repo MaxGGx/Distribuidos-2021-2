940b5b035d7c225d31ceb919a7a497737e1ad4d4
_MaxX_X <54334537+MaxGGx@users.noreply.github.com>
2021-10-29 01:02:42 -0300

    Create main.go

---
+++ M1/Test3/client/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	pb "github.com/MaxGGx/Distribuidos-2021-2/tree/main/M1/Test3/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		panic("No se puede conectar al servidor "+ err.Error())
+	}
+
+	serviceClient := pb.NewEntradaMensajeClient(conn)
+
+	res, err := serviceClient.Intercambio(context.Background(), &pb.Mensaje{
+		Body: "Mensaje de prueba desde cliente",
+	})
+
+	if err != nil {
+		panic("Mensaje no pudo ser creado ni enviado: "+ err.Error())
+	}
+
+	fmt.Println(res.Body)
+}
