1fbc6e7c27dceeba2365bbb54417b33060bf49b3
_MaxX_X <54334537+MaxGGx@users.noreply.github.com>
2021-10-30 01:50:52 -0300

    Update main.go

--- M1/TestRabbitMQ/main.go
+++ M1/TestRabbitMQ/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+)
 
 func main() {
 	fmt.Println("Test RabbitMQ")
+
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Si lees esto es porque se ejecuto bien la conexion a la instancia de RabbitMQ")
 }
