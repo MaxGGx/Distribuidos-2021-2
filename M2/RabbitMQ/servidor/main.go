4d632f246f88278a9a7b7ef1ecf8e9d32eaabcb4
ramm1024 <luis.gonzalezar@sansano.usm.cl>
2021-10-30 11:17:12 -0300

    borrador pozo
    Primer  intento de main.go en M2 para hacer el poso, incluyendo go.mod y go.sum

---
+++ M2/RabbitMQ/servidor/main.go
@@ -0,0 +1,54 @@
+package main
+
+import(
+	"fmt"
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	pozo = 0
+
+	fmt.Println("Aplicacion consumidor (Server)")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer conn.Close()
+
+	ch,err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer ch.Close()
+
+	msgs, err :=  ch.Consume(
+		"TestQueue",
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	forever := make(chan bool)
+	go func() {
+		for d := range msgs{
+			fmt.Printf("Mensaje recibido: %s\n", d.Body)
+			//placeholder lógica
+			if d.Body == 1{
+				fmt.Printf("Pozo es: %s\n",pozo)
+			}
+			else if d.Body == 2{
+				pozo += 100000000
+			}
+		}
+	}()
+
+	fmt.Println("Conectado a la instancia de RabbitMQ")
+	fmt.Println("[*] - Esperando mensajes")
+
+	<-forever
+}
