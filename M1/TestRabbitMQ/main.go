0eb7aec654b37cc30e50b6ef7f04f6778105b22e
_MaxX_X <54334537+MaxGGx@users.noreply.github.com>
2021-10-30 01:58:04 -0300

    Update main.go

--- M1/TestRabbitMQ/main.go
+++ M1/TestRabbitMQ/main.go
@@ -17,4 +17,44 @@ func main() {
 	defer conn.Close()
 
 	fmt.Println("Si lees esto es porque se ejecuto bien la conexion a la instancia de RabbitMQ")
+
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"TestQueue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil{
+		fmt.Println(err)
+		panic(err)
+	}
+	fmt.Println(q)
+
+	err = ch.Publish(
+		"",
+		"TestQueue",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body: []byte("Hola Mundo")
+		}
+	)
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	fmt.Println("Si lees este otro mensaje, quiere decir que se publico un mensaje exitosamente en la cola TestQueue")
 }
