149a12707fe4a8595f19c92c969c0f88bc8a5a58
ramm1024 <luis.gonzalezar@sansano.usm.cl>
2021-11-05 04:10:41 -0300

    Test3 added

---
+++ M1/Test3/client/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	pb "github.com/MaxGGx/Distribuidos-2021-2/M1/Test3/proto"
+	"google.golang.org/grpc"
+)
+
+func msg(direccion string){
+	fmt.Printf("%s\n","localhost:"+direccion)
+	conn, err := grpc.Dial("localhost:"+direccion, grpc.WithInsecure())
+
+	if err != nil {
+		panic("No se puede conectar al servidor "+ err.Error())
+	}
+
+	serviceClient := pb.NewEntradaMensajeClient(conn)
+
+	res, err := serviceClient.Intercambio(context.Background(), &pb.Mensaje{
+		Body: "POZO",
+	})
+
+	if err != nil {
+		panic("Mensaje no pudo ser creado ni enviado: "+ err.Error())
+	}
+	fmt.Printf("----------------\n"
+	fmt.Println(res.Body)
+	fmt.Printf("----------------\n"
+}
+
+func main() {
+	//var srvrs = []string{"50051","50052","50053","50054","50055","50056","50057","50058","50059","50060","50061","50062","50063","50064","50065","50066"}
+	//msg("50051")
+	//msg("50052")
+	msg("50060")
+	//for i := 0; i < 15; i++ {
+	//	msg(srvrs[i])
+	//}
+	/*
+	conn, err := grpc.Dial("localhost"+":50051", grpc.WithInsecure())
+
+	if err != nil {
+		panic("No se puede conectar al servidor "+ err.Error())
+	}
+
+	serviceClient := pb.NewEntradaMensajeClient(conn)
+
+	res, err := serviceClient.Intercambio(context.Background(), &pb.Mensaje{
+		Body: "Enviando prueba...",
+	})
+
+	if err != nil {
+		panic("Mensaje no pudo ser creado ni enviado: "+ err.Error())
+	}
+
+	fmt.Println(res.Body)
+	*/
+}
