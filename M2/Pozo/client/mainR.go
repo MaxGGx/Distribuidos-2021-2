067a3a6ac3da3c898f02992197b87175ef09ccda
ramm1024 <luis.gonzalezar@sansano.usm.cl>
2021-11-04 23:58:11 -0300

    Update mainR.go

--- M2/Pozo/client/mainR.go
+++ M2/Pozo/client/mainR.go
@@ -10,8 +10,8 @@ import (
 func main() {
 	fmt.Println("Test RabbitMQ")
 
-	//conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	conn, err := amqp.Dial("amqp://guest:guest@dist33:5672/")
+	//conn, err := amqp.Dial("amqp://g@uest:guestlocalhost:5672/")
+	conn, err := amqp.Dial("amqp://prueba:prueba@dist33:5672/")
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
